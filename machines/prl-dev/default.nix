80c1697fe4b1c532b8dffcd7616fb0e4971f38da
Matvei Smirnov <matt@MacBook-Pro.attlocal.net>
2025-08-08 17:25:34 -0500

    prl-dev default upd

--- machines/prl-dev/default.nix
+++ machines/prl-dev/default.nix
@@ -1,40 +1,34 @@
-# machine specific configuration goes here
+# Parallels VM‑specific configuration
+{ lib, pkgs, ... }:
 {
-  lib,
-  inputs,
-  system,
-  config,
-  pkgs,
-  ...
-}:
-{
-  imports = [
-    ./hardware-configuration.nix
-  ];
+  imports = [ ./hardware-configuration.nix ];
 
   # Hostname
   networking.hostName = "prl-dev";
-  
-  hardware.opengl.enable = true;
-  hardware.opengl.extraPackages = [ 
+
+  # OpenGL / video acceleration (Parallels exposes virtio‑gpu)
+  hardware.opengl = {
+    enable         = true;
+    extraPackages  = [
       pkgs.mesa.drivers
       pkgs.vaapiVdpau
       pkgs.libvdpau-va-gl
-  ];
+    ];
+  };
+  # `hardware.opengl.driSupport` was removed in 24.05 → no longer needed.
 
-  hardware.opengl.driSupport = true;
-  
+  # Bootloader
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  
-  sound.enable = true;
+
+  # Modern audio stack (PipeWire). Old `sound.enable` is deprecated.
   hardware.pulseaudio.enable = false;
   services.pipewire = {
-  	enable = true;
-	  alsa.enable = true;
-	  alsa.support32Bit = true;
-	  pulse.enable = true;
-	  wireplumber.enable = true;
+    enable          = true;
+    alsa.enable     = true;
+    alsa.support32Bit = true;
+    pulse.enable    = true;
+    wireplumber.enable = true;
   };
 
   services.upower.enable = true;
