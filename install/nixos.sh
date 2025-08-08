92a27ef451e4fd41765a5f7d98303248d3e63dd4
Matvei Smirnov <matt@MacBook-Pro.attlocal.net>
2025-08-08 16:20:17 -0500

    nixFlakes upd

--- install/nixos.sh
+++ install/nixos.sh
@@ -49,7 +49,7 @@ swapon "/dev/${device}2"
 #echo "Generating config."
 #nixos-generate-config --root /mnt
 
-nix-env -iA nixpkgs.nixVersions.git
+nix-env -iA nixos.nixVersions.git
 nix-env -iA nixos.nixVersions.stable
 
 git clone https://github.com/andreaugustoaragao/nix-config
