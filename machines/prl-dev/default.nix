9804f3ee515d4af504494a96a81911b49a281bb4
Matvei Smirnov <matt@Matveis-MacBook-Pro.attlocal.net>
2025-08-09 13:16:31 -0500

    test

--- machines/prl-dev/default.nix
+++ machines/prl-dev/default.nix
@@ -1,10 +1,13 @@
-# Parallels VM (Apple Silicon / aarch64)
+# Parallels VM (aarch64) — GNOME на Wayland, без i3
 { lib, pkgs, ... }:
 {
   imports = [ ./hardware-configuration.nix ];
 
   networking.hostName = "prl-dev";
 
+  machine.x11.enable = lib.mkForce false;
+
+  # Графика
   hardware.graphics = {
     enable = true;
     extraPackages = [
@@ -14,9 +17,11 @@
     ];
   };
 
+  # Загрузчик
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  # Аудио: PipeWire
   hardware.pulseaudio.enable = false;
   services.pipewire = {
     enable = true;
@@ -38,6 +43,12 @@
   services.xserver.displayManager.gdm.wayland = true;
   services.xserver.desktopManager.gnome.enable = true;
 
+  services.xserver.displayManager.defaultSession = "gnome";
+
   services.greetd.enable = lib.mkForce false;
-  services.gvfs.enable = lib.mkForce true;
+  services.gvfs.enable  = lib.mkForce true;
+  programs.dconf.enable = true;
+
+  xdg.portal.enable = true;
+  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gnome ];
 }
